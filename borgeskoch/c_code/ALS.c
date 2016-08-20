ead240774dc20a1b12e88563534a7353d517c77c
tiko <tiko@ime.usp.br>
2016-08-20 08:32:06 -0300

    scanning file works

--- borgeskoch/c_code/ALS.c
+++ borgeskoch/c_code/ALS.c
@@ -2,52 +2,83 @@
 // v - which latent feature a song contains
 // u - extend to witch a user likes latent factors
 
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include "gl_avltreehash_list.h"
 
-struct entry {
-    char* music;
-    char* user;
+struct rating {
+    char *song;
+    char *user;
     int count;
 };
 
-int compareuser(const void *e1p, const void *e2p)
+int compareuser(const void *r1p, const void *r2p)
 {
-    const struct entry *e1, *e2;
-    int last, first;
+    const struct rating *r1, *r2;
 
-    e1 = (const struct employee *) e1p;
-    e2 = (const struct employee *) e2p;
+    r1 = (const struct rating *) r1p;
+    r2 = (const struct rating *) r2p;
 
-    if ((last = strcmp(e1->user, e2->user)) != 0)
-        return last;
+    if ( strcmp(r1->user, r2->user) != 0 )
+        return 1;
 
+    return 0;
+}
+
+struct rating *parse_input_line(char *line)
+{
+    char *token;
+    struct rating *rat = malloc(sizeof(struct rating));
     
+    token = strsep(&line, "\t");
+    rat->user = strdup(token);            
+    
+    token = strsep(&line, "\t");
+    rat->song = strdup(token);            
 
+    token = strsep(&line, "\t");
+    rat->count = atoi(token);            
+
+    return rat;
+}
+
+void print_rating(struct rating *r)
+{
+    printf("%s\t%s\t%d\n", r->user, r->song, r->count);
 }
 
 int main(int argc, char *argv[])
 {
-    void *musicroot = NULL;
-    void *userroot = NULL;
+    //void *musicroot = NULL;
+    //void *userroot = NULL;
 
     FILE *ifilestream;
+    char *line = NULL;
     size_t len = 0;
     ssize_t read;
 
+    struct rating *r;
 
-    if ((ifilestream = fopen(iargv[1], "r")) == NULL)
+    if ((ifilestream = fopen(argv[1], "r")) == NULL)
         fprintf(stderr, "can't open %s", argv[1]);
+    
 
     while ((read = getline(&line, &len, ifilestream)) != -1) {
-
-        // split line in user, song and count
         
+        // split line in user, song and count
+        r = parse_input_line(line);
+        
+        print_rating(r);
+
         //add song + count to userdb (avl tree)
+        
+        
         //add user + count to musicdb (avl tree)
         //
-    
     }
+    
+    free(line);
 
-
+    exit(EXIT_SUCCESS);
 }
